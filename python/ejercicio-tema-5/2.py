96de1e5e92f588ad64eaeecaf15762887fc180b7
alejocoder <alejosamp@gmail.com>
2022-07-08 10:01:52 -0500

    ejercicio 2 tema 5 python

---
+++ python/ejercicio-tema-5/2.py
@@ -0,0 +1,15 @@
+def primo(numero):
+    if numero == 0 or numero == 1 or numero == 4:
+        return False
+    for x in range(2, int(numero/2)):
+        if numero % x == 0:
+            return False
+    return True
+
+# Probar
+numero = int(input("Dame un número: "))
+es_primo = primo(numero)
+if es_primo:
+	print("Es primo")
+else:
+	print("NO es primo")
