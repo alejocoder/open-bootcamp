acbe7d606466965dfcde10b85f124994ddddbbce
alejandro <alejosamp@gmail.com>
2022-09-20 08:34:16 -0500

    modulo 2 js

---
+++ Javascript/2.sintaxis,variables y palabras reservadas/tipado y tipos primitivos.js
@@ -0,0 +1,16 @@
+/*
+tipado inferido, no hay que especificar el tipo.
+
+tipos:
+    primitivos
+        - number
+        - string
+        - boolean
+        - null --> vacio pero con espacio en memoria
+        - undefined --> no está asignado a un espacio en memoria
+
+
+null, undefined, false y 0 --> son falsy
+
+
+*/ 
---
+++ Javascript/ejercicios/modulo2.js
@@ -0,0 +1,10 @@
+mi_nombre= "Hector Alejandro Jimenez Lopez";
+mi_edad= 21;
+soy_desarrollador= true;
+fecha_de_nacimiento= newDate(2022, 4, 10);
+libro_favorito={
+    titulo: "la isla misteriosa",
+    autor: "julio verne",
+    fecha: newDate(1875, 10, 22),
+    url: "https://es.wikipedia.org/wiki/La_isla_misteriosa"
+};
