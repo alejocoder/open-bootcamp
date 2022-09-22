1114df6455af6b0b4a56ec01ae493ae6f0020849
alejandro <alejosamp@gmail.com>
2022-09-22 10:36:53 -0500

    ejercicio numbers js

---
+++ Javascript/ejercicios/numbers.js
@@ -0,0 +1,14 @@
+let altura= 200;
+let alturaMetros = 2.0;
+let peso = 117.5;
+
+let alturaRedondeada= alturaMetros.toFixed(2);
+console.log(alturaRedondeada);
+
+let pesoRedondeado = peso.toPrecision(3);
+console.log(pesoRedondeado);
+
+
+let maximoValor = Number.MAX_VALUE;
+
+console.log(maximoValor +1);
