89e8e95837ccd9411272a581502351a7efc64ab7
alejandro <alejosamp@gmail.com>
2022-09-25 15:51:18 -0500

    ejercicio 1 modulo 7 js

---
+++ Javascript/ejercicios/conjuntos.js
@@ -0,0 +1,11 @@
+let conjuntos = ["edgar", "emiliano", "felipe", "soraya", "martina", "hector"];
+
+let setConjuntos = new Set(conjuntos);
+
+console.log(setConjuntos);
+
+setConjuntos.add("hector");
+console.log(setConjuntos);
+
+setConjuntos.add("javascript");
+console.log(setConjuntos);
